d4f68d066c60ff0841802e3a4fb8283144aa2636
Eduarda Lima <eududalim@gmail.com>
2020-08-27 19:26:32 -0300

    Creater tests for creation of users to firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,23 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gerente_loja/models/user_admin_model.dart';
+import 'package:gerente_loja/repositories/user_admin_repo.dart';
 
 void main() {
-  test('', () {});
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  final _repoAdmin = UserAdminRepo();
+  final _user = UserAdminModel(
+      name: 'eduarda lima',
+      nameStore: 'loja da duda',
+      cpf: '0021516220',
+      email: 'teste@teste.com',
+      password: '12345678');
+
+  test(
+      'Deve criar um usuario no firebase e salvar seus dados no Banco de Dados',
+      () async {
+    expect(await _repoAdmin.createUser(_user), true);
+  });
 }
